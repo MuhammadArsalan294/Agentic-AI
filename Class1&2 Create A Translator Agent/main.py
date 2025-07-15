23a5cc8a178e90b235925810cdfed33e08b3333d
MuhammadArsalan294 <164243415+MuhammadArsalan294@users.noreply.github.com>
2025-07-15 18:13:08 +0500

    Add files via upload

--- Class1&2 Create A Translator Agent/main.py
+++ Class1&2 Create A Translator Agent/main.py
@@ -1,3 +1,52 @@
+#CLASSWORK
+"""
+from agents import Agent, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig, Runner
+import os
+from dotenv import load_dotenv
+
+# Load the environment variables from the .env file
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+# print(gemini_api_key) # python main.py
+
+# Check if the API key is present; if not, raise an error
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="models/gemini-1.5-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent = Agent(
+    name="Writer Agent",
+    instructions="Your task is to answer user query. If you do not have the answer to the user query just deny. Do not hallucinate.",
+)
+
+response = Runner.run_sync(
+    agent,
+    input = 'What is the next.js app router? ',
+    run_config = config 
+)
+print(response)
+# OR print(response.final_output)
+"""
+
+
+# ASSIGNMENT
 from dotenv  import load_dotenv
 import os
 from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
