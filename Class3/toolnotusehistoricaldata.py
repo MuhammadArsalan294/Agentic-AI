a710d14038994e1dfbfd537ff76e6bd4ce9feb0b
MuhammadArsalan294 <164243415+MuhammadArsalan294@users.noreply.github.com>
2025-07-15 18:14:26 +0500

    Add files via upload

---
+++ Class3/toolnotusehistoricaldata.py
@@ -0,0 +1,21 @@
+from agents import Agent, Runner
+from main import config # import module ye main file sy import kia
+
+
+agent = Agent(
+    name = "General Agent",
+    instructions = "You are a helpful assistant. Your task is to help the user with their queries"
+)
+
+result = Runner.run_sync(
+    agent,
+    'Who is the founder of Pakistan', # Historical Data
+    #'What is the conversion rate of USD to PKR', # Real time data ye answer nhi dy ga sorry kr ly ga ya purana data dy ga  answer k liye humein tool calling ka use krna hoga
+    #'Show me the top 10 student of class 10', # Personalised data ye answer nhi dy ga sorry kr ly ga ya purana data dy ga  answer k liye humein tool calling ka use krna hoga
+    run_config=config
+)
+
+# Print the translated result
+print(result.final_output)
+
+# uv run toolhistoricaldata.py
---
+++ Class3/weatherapikey.py
@@ -0,0 +1,29 @@
+from agents import Agent, Runner, function_tool
+from main import config # import module ye main file sy import kia
+
+import requests
+
+
+@function_tool # Decorator lga kar function ko tool bna dia or upper es ko import bhi kia hai
+def get_weather(city: str): # ye function bnaya hai
+    response = requests.get(f"https://wttr.in/{city}?format=3")
+    print("-----API RESULT-----")
+    print(response.text)
+    return response.text  # real-time weather from wttr.in
+    
+
+agent = Agent(
+    name = "Weather Agent",
+    instructions = "You are a helpful assistant. Your task is to help the user with their queries",
+    tools = [get_weather] # ye upper sy aya hai or yaha function ka name likha hai 
+)
+
+result = Runner.run_sync(agent,
+                        'What is the current weather in Karachi today?',
+                        run_config=config
+)
+
+# Print the translated result
+print(result.final_output)
+
+# uv run weatherapikey.py
